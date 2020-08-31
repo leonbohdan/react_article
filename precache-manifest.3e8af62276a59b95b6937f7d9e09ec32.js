71735ffcbe7c248c537eeb340990a0a93524c972
Bohdan Leonets <leonetsbogdan@gmail.com>
2020-08-31 22:39:30 +0300

    Updates

---
+++ precache-manifest.3e8af62276a59b95b6937f7d9e09ec32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f9dea1a9846863517380042fb3107c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2db50238efcbad84ce63",
+    "url": "./static/css/main.0acc2731.chunk.css"
+  },
+  {
+    "revision": "0ebb41580dd51017fc25",
+    "url": "./static/js/2.dd291650.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.dd291650.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2db50238efcbad84ce63",
+    "url": "./static/js/main.0f24265c.chunk.js"
+  },
+  {
+    "revision": "994f72ec58672ee8392d",
+    "url": "./static/js/runtime-main.a5fa282b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.575c894ccaaaec018efa3e6d1c5de3fa.js"
+  "./precache-manifest.3e8af62276a59b95b6937f7d9e09ec32.js"
 );
 
 self.addEventListener('message', (event) => {
